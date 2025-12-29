6746b45a942c66e074bc569c28dc9f80adcbb482
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2025-12-29 08:57:01 -0500

    Setup i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://williamlafortunecaissy.github.io',
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['en', 'fr'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  fr: 'Français',
+  en: 'English',
+};
+
+export const defaultLang = 'fr';
+export const showDefaultLang = false;
+
+
+export const ui = {
+  fr: {
+    'welcome.title': 'Récré EauSecours',
+  },
+  en: {
+    'welcome.title': 'Splash rescue',
+  },
+} as const;
